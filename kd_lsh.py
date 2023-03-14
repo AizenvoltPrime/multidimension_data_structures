5ac91cc56e2a041670fcbe3a8a22f14b22bd45f1
Alex <alex11737@gmail.com>
2023-03-14 20:04:46 +0200

    added missing libraries

--- kd_lsh.py
+++ kd_lsh.py
@@ -1,7 +1,12 @@
 # Import libraries
 import pandas as pd
+import numpy as np
+
+# Read data from scrapdata.csv
 from sklearn.neighbors import KDTree
 from sklearn.preprocessing import LabelEncoder
+from sklearn.feature_extraction.text import TfidfVectorizer
+from datasketch import MinHashLSHForest, MinHash,MinHashLSH
 
 # Read data from scrapdata.csv
 data = pd.read_csv("scrapdata.csv", header=None, names=["surname", "awards", "education"])
@@ -23,30 +28,30 @@ def query_kd_tree(range_low, range_high, num_awards):
     return result.iloc[:, :3]
 
 print(query_kd_tree("A", "R", 0))
-# # Convert education to vector representation using TF-IDF 
-# vectorizer = TfidfVectorizer() # Create vectorizer object
-# Y = vectorizer.fit_transform(data["education"]) # Fit and transform education texts
-# # Apply MinHash on vectors to create hash signatures 
-# lsh = MinHashLSH(threshold=0.2) # Create MinHashLSH object with similarity threshold (you can change this)
-# for i in range(Y.shape[0]): # Loop over each vector 
-#     mh = MinHash(num_perm=128) # Create MinHash object with 10 permutations (you can change this)
-#     for j in Y[i].nonzero()[1]: # Loop over each non-zero element in vector 
-#         mh.update(str(j).encode('utf8')) # Update MinHash with element value encoded as bytes 
-#         lsh.insert(i, mh, check_duplication=False) # Insert index and MinHash into LSH
+# Convert education to vector representation using TF-IDF 
+vectorizer = TfidfVectorizer() # Create vectorizer object
+Y = vectorizer.fit_transform(data["education"]) # Fit and transform education texts
+# Apply MinHash on vectors to create hash signatures 
+lsh = MinHashLSH(threshold=0.2) # Create MinHashLSH object with similarity threshold (you can change this)
+for i in range(Y.shape[0]): # Loop over each vector 
+    mh = MinHash(num_perm=128) # Create MinHash object with 10 permutations (you can change this)
+    for j in Y[i].nonzero()[1]: # Loop over each non-zero element in vector 
+        mh.update(str(j).encode('utf8')) # Update MinHash with element value encoded as bytes 
+        lsh.insert(i, mh, check_duplication=False) # Insert index and MinHash into LSH
 
-# # Define a function that returns points based on query vector and similarity percentage (not needed anymore)
-# def query_lsh(vector):
-#     mh_query = MinHash(num_perm=128) # Create MinHash object for query vector 
-#     for j in vector.nonzero()[1]: # Loop over each non-zero element in vector 
-#         mh_query.update(str(j).encode('utf8')) # Update MinHash with element value encoded as bytes 
-#         result = lsh.query(mh_query) # Query LSH with query MinHash and get result as a list of indices 
-#     return result 
+# Define a function that returns points based on query vector and similarity percentage (not needed anymore)
+def query_lsh(vector):
+    mh_query = MinHash(num_perm=128) # Create MinHash object for query vector 
+    for j in vector.nonzero()[1]: # Loop over each non-zero element in vector 
+        mh_query.update(str(j).encode('utf8')) # Update MinHash with element value encoded as bytes 
+        result = lsh.query(mh_query) # Query LSH with query MinHash and get result as a list of indices 
+    return result 
 
-# # Combine both functions to answer queries of the form: "Find computer scientists from scrapdata.csv with >60% education similarity, whose letter is in the interval [A, G], and who have won > 4 awards."
-# def query_combined(range_low,range_high,threshold):
-#     result1 = query_kd_tree(range_low,range_high, threshold) # Query k-d tree first 
-#     result2 = query_lsh(Y[result1.index]) # Query LSH on the subset of result1  
-#     return data.iloc[result2] # Return final result as a pandas dataframe 
+# Combine both functions to answer queries of the form: "Find computer scientists from scrapdata.csv with >60% education similarity, whose letter is in the interval [A, G], and who have won > 4 awards."
+def query_combined(range_low,range_high,threshold):
+    result1 = query_kd_tree(range_low,range_high, threshold) # Query k-d tree first 
+    result2 = query_lsh(Y[result1.index]) # Query LSH on the subset of result1  
+    return data.iloc[result2] # Return final result as a pandas dataframe 
 
-# # Test with an example query 
-# print(query_combined("A","Z", 1)) # Remove 4 from the arguments
+# Test with an example query 
+print(query_combined("A","Z", 1)) # Remove 4 from the arguments
