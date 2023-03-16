a53a6d669308551da100a57973f837d7f3033dc0
Alex <alex11737@gmail.com>
2023-03-16 18:40:04 +0200

    lsh kd_tree implementation works probably
    Co-authored-by: PudicusAqua <PudicusAqua@users.noreply.github.com>

--- kd_lsh.py
+++ kd_lsh.py
@@ -27,12 +27,11 @@ def query_kd_tree(range_low, range_high, num_awards):
     result = result[result['awards'] >= num_awards]
     return result.iloc[:, :3]
 
-print(query_kd_tree("A", "R", 0))
 # Convert education to vector representation using TF-IDF 
 vectorizer = TfidfVectorizer() # Create vectorizer object
 Y = vectorizer.fit_transform(data["education"]) # Fit and transform education texts
 # Apply MinHash on vectors to create hash signatures 
-lsh = MinHashLSH(threshold=0.2) # Create MinHashLSH object with similarity threshold (you can change this)
+lsh = MinHashLSH(threshold=0.4) # Create MinHashLSH object with similarity threshold (you can change this)
 for i in range(Y.shape[0]): # Loop over each vector 
     mh = MinHash(num_perm=128) # Create MinHash object with 10 permutations (you can change this)
     for j in Y[i].nonzero()[1]: # Loop over each non-zero element in vector 
@@ -44,14 +43,25 @@ def query_lsh(vector):
     mh_query = MinHash(num_perm=128) # Create MinHash object for query vector 
     for j in vector.nonzero()[1]: # Loop over each non-zero element in vector 
         mh_query.update(str(j).encode('utf8')) # Update MinHash with element value encoded as bytes 
-        result = lsh.query(mh_query) # Query LSH with query MinHash and get result as a list of indices 
+        result = lsh.query(mh_query) # Query LSH with query MinHash and get result as a list of indices
     return result 
 
 # Combine both functions to answer queries of the form: "Find computer scientists from scrapdata.csv with >60% education similarity, whose letter is in the interval [A, G], and who have won > 4 awards."
 def query_combined(range_low,range_high,threshold):
     result1 = query_kd_tree(range_low,range_high, threshold) # Query k-d tree first 
     result2 = query_lsh(Y[result1.index]) # Query LSH on the subset of result1  
-    return data.iloc[result2] # Return final result as a pandas dataframe 
+    final_result = []
+    print(result1)
+    print(result2)
+    for i in range(result1.shape[0]):
+        if result1.iloc[i,1:1].name in result2:
+            final_result.append(result1.iloc[i].tolist())
+    return final_result # Return final result as a pandas dataframe 
 
-# Test with an example query 
-print(query_combined("A","Z", 1)) # Remove 4 from the arguments
+# Test with an example query
+test = query_combined("K","O", 10) # Remove 4 from the arguments
+
+for i in test:
+    print(i,"\n\n")
+    
+#[67, 68, 102, 70, 44, 47, 48, 52, 85]
