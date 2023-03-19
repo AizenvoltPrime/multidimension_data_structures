f9a7a00dcd0410bbe634f30a3a1d278fedc1baf6
Alex <alex11737@gmail.com>
2023-03-19 18:55:58 +0200

    there is still a bug
    Co-authored-by: PudicusAqua <PudicusAqua@users.noreply.github.com>

--- kd_lsh.py
+++ kd_lsh.py
@@ -10,6 +10,12 @@ from datasketch import MinHashLSHForest, MinHash,MinHashLSH
 from sklearn.cluster import DBSCAN
 from sklearn.preprocessing import normalize
 
+first_letter = input("Enter first letter: ")
+last_letter = input("Enter last letter: ")
+awards = int(input("Enter number of awards: "))
+sim_threshold = int(input("Enter threshold: "))
+sim_threshold /= 100
+
 # Read data from scrapdata.csv
 data = pd.read_csv("scrapdata.csv", header=None, names=["surname", "awards", "education"])
 
@@ -26,16 +32,18 @@ def query_kd_tree(range_low, range_high, num_awards):
     result = data.iloc[ind]
     result = result[result['first_letter'] >= range_low[0].upper()]
     result = result[result['first_letter'] <= range_high[0].upper()]
-    result = result[result['awards'] >= num_awards]
+    result = result[result['awards'] > num_awards]
     return result.iloc[:, :3]
 
-lsh_builder = query_kd_tree("E", "R", 1)
+lsh_builder = query_kd_tree(first_letter, last_letter, awards)
+
+print("The LSH indexes are: ", lsh_builder, "\n\n\n\n")
 
 # Convert education to vector representation using TF-IDF 
 vectorizer = TfidfVectorizer() # Create vectorizer object
 Y = vectorizer.fit_transform(lsh_builder.iloc[:,2]) # Fit and transform education texts
 # Apply MinHash on vectors to create hash signatures 
-lsh = MinHashLSH(threshold = 0.2) # Create MinHashLSH object
+lsh = MinHashLSH(threshold = sim_threshold) # Create MinHashLSH object
 for i in range(Y.shape[0]): # Loop over each vector 
     mh = MinHash(num_perm=128) # Create MinHash object with 10 permutations (you can change this)
     for j in Y[i].nonzero()[1]: # Loop over each non-zero element in vector 
@@ -54,4 +62,19 @@ def query_lsh(matrix):
     return results 
 
 final_result = query_lsh(Y)
-print(final_result)
+print("The groups of similarities are: ", final_result, "\n\n\n\n")
+
+count = 0
+for i in range(len(final_result)):
+    if len(final_result[i]) > 1:
+        print("\n\n")
+        if i == 0:
+            print("The scientist with the name", data['surname'][lsh_builder.index[i]], "is similar with:   ")
+        elif i == 1:
+            print("The scientist with the name", data['surname'][lsh_builder.index[i]], "is similar with:   ")
+        elif i == 2:
+            print("The scientist with the name", data['surname'][lsh_builder.index[i]], "is similar with:   ")
+        else:
+            print("The scientist with the name", data['surname'][lsh_builder.index[i]], "is similar with:   ")
+        for j in range(len(final_result[i])):
+            print(data['surname'][lsh_builder.index[final_result[i][j]]]," \t| " ,data['awards'][lsh_builder.index[final_result[i][j]]]," \t| ",data['education'][lsh_builder.index[final_result[i][j]]], "\n\n")
