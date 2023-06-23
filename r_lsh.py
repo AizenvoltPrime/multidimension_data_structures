b4072625a846e014c3dc58f30eb1683aedb91785
Alex <alex11737@gmail.com>
2023-06-23 16:42:58 +0300

    fixed r_lsh

--- r_lsh.py
+++ r_lsh.py
@@ -1,8 +1,8 @@
 import pandas as pd
-from rtree import index
+from rtree import Index as RTree
 from sklearn.preprocessing import LabelEncoder
 from sklearn.feature_extraction.text import TfidfVectorizer
-from datasketch import MinHash,MinHashLSH
+from datasketch import MinHash, MinHashLSH
 
 # Get user inputs for first letter, last letter, number of awards and similarity threshold
 first_letter = input("Enter first letter: ")
@@ -14,38 +14,39 @@ sim_threshold /= 100
 # Read data from scrapdata.csv
 data = pd.read_csv("scrapdata.csv", header=None, names=["surname", "awards", "education"])
 
-# Build an R-tree using surname and awards
+# Build a R-tree using surname and awards
 le = LabelEncoder()
-data['first_letter'] = data['surname'].str[0]
-X = data[["surname", "awards"]].values # Convert to numpy array
-X[:,0] = le.fit_transform(X[:,0]) # Transform surname column
+data['first_letter'] = data['surname'].str[0].str.upper()
+X = data[["first_letter", "awards"]].values # Convert to numpy array
 
-p = index.Property()
-idx = index.Index(properties=p)
+le.fit(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+X[:,0] = le.transform(X[:,0]) # Transform first_letter column
+
+rtree = RTree()
+
 for i in range(len(X)):
-    idx.insert(i, (X[i][0], X[i][1], X[i][0], X[i][1]))
+    x, y = X[i]
+    rtree.insert(i, (x, y, x, y))
 
 def query_r_tree(range_low, range_high, num_awards):
     # Query the R-tree to find surnames within the given range and with more than the given number of awards
-    low = ord(range_low[0].upper())
-    high = ord(range_high[0].upper())
-    result_indices = list(idx.intersection((low,num_awards+1,high,float('inf'))))
-    result = data.iloc[result_indices]
-    result = result[result['first_letter'] >= range_low[0].upper()]
-    result = result[result['first_letter'] <= range_high[0].upper()]
-    result = result[result['awards'] > num_awards]
+    low = le.transform([range_low])[0]
+    high = le.transform([range_high])[0]
+    query_r = (low, num_awards+1, high, data['awards'].max())
+    matches = list(rtree.intersection(query_r))
+    result = data.iloc[list(matches)]
     return result.iloc[:, :3]
 
-lsh_builder=query_r_tree(first_letter,last_letter,awards)
+r_tree_builder = query_r_tree(first_letter.upper(), last_letter.upper(), awards)
 
-print("The R-tree query results are: \n", lsh_builder)
+print("The R-tree query results are: \n", r_tree_builder)
 
 # Convert education to vector representation using TF-IDF 
 vectorizer = TfidfVectorizer() # Create vectorizer object
-Y = vectorizer.fit_transform(lsh_builder.iloc[:,2]) # Fit and transform education texts
+Y = vectorizer.fit_transform(r_tree_builder.iloc[:,2]) # Fit and transform education texts
 
 # Apply MinHash on vectors to create hash signatures 
-lsh = MinHashLSH(threshold = sim_threshold) # Create MinHashLSH object
+lsh = MinHashLSH(threshold=sim_threshold) # Create MinHashLSH object
 for i in range(Y.shape[0]): # Loop over each vector 
     mh = MinHash(num_perm=128) # Create MinHash object with 10 permutations (you can change this)
     for j in Y[i].nonzero()[1]: # Loop over each non-zero element in vector 
@@ -64,19 +65,19 @@ def query_lsh(matrix):
     return results 
 
 final_result = query_lsh(Y)
-print("The groups of similarities are: ", final_result, "\n\n\n\n")
+print("The groups of similarities are: ", final_result)
 
 count = 0
 for i in range(len(final_result)):
     if len(final_result[i]) > 1:
         print("\n\n")
         if i == 0:
-            print("The scientist with the name", data['surname'][lsh_builder.index[i]], "is similar with: ")
+            print("The scientist with the name", data['surname'][r_tree_builder.index[i]], "is similar with: ")
         elif i == 1:
-            print("The scientist with the name", data['surname'][lsh_builder.index[i]], "is similar with: ")
+            print("The scientist with the name", data['surname'][r_tree_builder.index[i]], "is similar with: ")
         elif i == 2:
-            print("The scientist with the name", data['surname'][lsh_builder.index[i]], "is similar with: ")
+            print("The scientist with the name", data['surname'][r_tree_builder.index[i]], "is similar with: ")
         else:
-            print("The scientist with the name", data['surname'][lsh_builder.index[i]], "is similar with: ")
+            print("The scientist with the name", data['surname'][r_tree_builder.index[i]], "is similar with: ")
         for j in range(len(final_result[i])):
-            print(data['surname'][lsh_builder.index[final_result[i][j]]]," \t| " ,data['awards'][lsh_builder.index[final_result[i][j]]]," \t| ",data['education'][lsh_builder.index[final_result[i][j]]], "\n\n")
+            print(data['surname'][r_tree_builder.index[final_result[i][j]]]," \t| " ,data['awards'][r_tree_builder.index[final_result[i][j]]]," \t| ",data['education'][r_tree_builder.index[final_result[i][j]]], "\n\n")
